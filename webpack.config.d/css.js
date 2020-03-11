ff8e746d14d9a13c18dc26acdf6dea7e9e5f310b
sandro <morpheus@rabbithole.ch>
2020-03-11 10:48:14 +0100

    Fixed style loading

--- webpack.config.d/css.js
+++ webpack.config.d/css.js
@@ -2,7 +2,5 @@ config.resolve.modules.push("processedResources/Js/main");
 
 config.module.rules.push({
     test: /\.css$/,
-    use: [
-        "css-loader" // translates CSS into CommonJS
-    ]
+    loader: 'style-loader!css-loader'
 });
