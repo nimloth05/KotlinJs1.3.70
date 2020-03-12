b78e1160b977f0ad09de115723a6b811f11942df
sandro <morpheus@rabbithole.ch>
2020-03-12 11:19:58 +0100

    Threejs example with a texture

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,13 +1,13 @@
 import ch.viseon.threejs.declarations.cameras.PerspectiveCamera
 import ch.viseon.threejs.declarations.geometries.BoxGeometry
-import ch.viseon.threejs.declarations.materials.MeshNormalMaterial
+import ch.viseon.threejs.declarations.loaders.TextureLoader
+import ch.viseon.threejs.declarations.materials.MeshBasicMaterial
 import ch.viseon.threejs.declarations.objects.Mesh
 import ch.viseon.threejs.declarations.renderers.WebGLRenderer
 import ch.viseon.threejs.declarations.scenes.Scene
 import kotlin.browser.document
 import kotlin.browser.window
 import kotlin.js.json
-
 
 fun main() {
     js("require('style.css');")
@@ -18,8 +18,10 @@ fun main() {
 
         val scene = Scene();
 
+        val texture = TextureLoader().load("textures/crate.gif")
+
         val geometry = BoxGeometry(0.2, 0.2, 0.2);
-        val material = MeshNormalMaterial();
+        val material = MeshBasicMaterial(json("map" to texture));
 
         val mesh = Mesh(geometry, material);
         scene.add(mesh);
@@ -27,9 +29,19 @@ fun main() {
         val renderer = WebGLRenderer()
         renderer.setSize(window.innerWidth, window.innerHeight);
         document.body?.appendChild(renderer.domElement);
+
+        onWindowResize(camera, renderer)
         render(mesh, scene, camera, renderer)
     }
 
+}
+
+fun onWindowResize(camera: PerspectiveCamera, renderer: WebGLRenderer) {
+    window.addEventListener("resize", {
+        camera.aspect = (window.innerWidth / window.innerHeight).toDouble()
+        camera.updateProjectionMatrix()
+        renderer.setSize(window.innerWidth, window.innerHeight)
+    })
 }
 
 fun render(mesh: Mesh, scene: Scene, camera: PerspectiveCamera, renderer: WebGLRenderer) {
